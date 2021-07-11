89b5975c5146005cdf17687ab7cddee0d66e948a
SimplyStaking <Staking7pc@gmail.com>
2021-07-11 08:32:44 +0100

    as of

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -78,7 +78,7 @@ function Card() {
     <>
       <div className="container mnn">
         <div className="cards-title">
-          Stats Summary As of {moment(stakedAmount.time).format("LL")}{" "}
+          Stats Summary as of {moment(stakedAmount.time).format("LL")}{" "}
           {moment(stakedAmount.time).format("LTS")} UTC
         </div>
         <Row className="cardsWrapper">
